6021de4ee748759aa7b96e5f71c5d69104f451df
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-06-30 12:01:48 +0200

    - prettier formats - delete examples

--- src/examples/general/store.ts
+++
@@ -1,9 +0,0 @@
-import Store from '../../store';
-import { State } from './types';
-
-export default new Store<State>({
-  users: [],
-  isFetching: false,
-  error: null,
-  editedUsername: null
-});
--- src/store/middlewares/thunk.ts
+++ src/store/middlewares/thunk.ts
@@ -1,10 +1,10 @@
-import { Middleware, GetState, Dispatch, Reducer, Store } from '../types';
+import { Middleware, GetState, Dispatch, Reducer, Store } from '../types'
 
 export interface ThunkMiddleware<State, ExtraArgument>
   extends Middleware<State> {
   withExtraArgument: (
     extraArgument: ExtraArgument
-  ) => ThunkMiddleware<State, ExtraArgument>;
+  ) => ThunkMiddleware<State, ExtraArgument>
 }
 
 export interface Delegate<State, ExtraArgument, T> {
@@ -12,28 +12,28 @@ export interface Delegate<State, ExtraArgument, T> {
     dispatch: ThunkDispatch<State, ExtraArgument>,
     getState: GetState<State>,
     extraArgument: ExtraArgument
-  ): T;
+  ): T
 }
 
 export interface ThunkDispatch<State, ExtraArgument>
   extends Dispatch<Reducer<State>> {
-  <T>(reducer: Thunk<State, ExtraArgument, T>): T;
+  <T>(reducer: Thunk<State, ExtraArgument, T>): T
 }
 
 export interface Thunk<State, ExtraArgument, T> {
-  (state: State): Delegate<State, ExtraArgument, T>;
+  (state: State): Delegate<State, ExtraArgument, T>
 }
 
 function thunkFactory<T>(extraArgument?: T): ThunkMiddleware<any, T> {
   const thunk = ((store, reducer) => {
-    const state = store.getState();
-    const result = reducer(state);
+    const state = store.getState()
+    const result = reducer(state)
     return typeof result === 'function'
       ? result(store.dispatch, store.getState, extraArgument)
-      : reducer;
-  }) as ThunkMiddleware<any, T>;
-  thunk.withExtraArgument = thunkFactory;
-  return thunk;
+      : reducer
+  }) as ThunkMiddleware<any, T>
+  thunk.withExtraArgument = thunkFactory
+  return thunk
 }
 
-export default thunkFactory();
+export default thunkFactory()
