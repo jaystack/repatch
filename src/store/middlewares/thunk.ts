c5377418013bd2098f7a7ac02200a2f7f1cdedb8
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2017-08-21 20:24:13 +0200

    refact middlewares

--- src/store/middlewares/thunk.ts
+++ src/store/middlewares/thunk.ts
@@ -1,29 +1,35 @@
 import { Middleware, GetState, Dispatch, Reducer, Store } from '../types';
 
-export interface ThunkMiddleware<State, ExtraArgument> extends Middleware<State> {
-  withExtraArgument: (extraArgument: ExtraArgument) => ThunkMiddleware<State, ExtraArgument>;
+export interface ThunkMiddleware<State, ExtraArgument>
+  extends Middleware<State, Reducer<State>, Thunk<State, ExtraArgument, any>> {
+  withExtraArgument: <EA>(extraArgument: EA) => ThunkMiddleware<State, EA>;
 }
 
-export interface Delegate<State, ExtraArgument, T> {
-  (dispatch: ThunkDispatch<State, ExtraArgument>, getState: GetState<State>, extraArgument: ExtraArgument): T;
+export interface Delegate<State, ExtraArgument, Return> {
+  (dispatch: ThunkDispatch<State, ExtraArgument>, getState: GetState<State>, extraArgument: ExtraArgument): Return;
 }
 
 export interface ThunkDispatch<State, ExtraArgument> extends Dispatch<Reducer<State>> {
-  <T>(reducer: Thunk<State, ExtraArgument, T>): T;
+  <Return>(reducer: Thunk<State, ExtraArgument, Return>): Return;
 }
 
-export interface Thunk<State, ExtraArgument, T> {
-  (state: State): Delegate<State, ExtraArgument, T>;
+export interface Thunk<State, ExtraArgument, Return> {
+  (state: State): Delegate<State, ExtraArgument, Return>;
 }
 
-function thunkFactory<T>(extraArgument?: T): ThunkMiddleware<any, T> {
-  const thunk = ((store, reducer) => {
+const thunkFactory = (extraArgument?) => {
+  const thunk = store => next => reducer => {
+    if (typeof reducer !== 'function') throw new Error('Thunk requires reducers as functions');
     const state = store.getState();
     const result = reducer(state);
-    return typeof result === 'function' ? result(store.dispatch, store.getState, extraArgument) : reducer;
-  }) as ThunkMiddleware<any, T>;
-  thunk.withExtraArgument = thunkFactory;
+    if (typeof result === 'function') return result(store.dispatch, store.getState, extraArgument);
+    else {
+      next(_ => result);
+      return reducer;
+    }
+  };
+  thunk['withExtraArgument'] = thunkFactory;
   return thunk;
-}
+};
 
-export default thunkFactory();
+export default thunkFactory() as ThunkMiddleware<any, any>;
